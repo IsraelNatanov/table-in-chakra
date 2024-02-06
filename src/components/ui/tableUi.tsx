1e2801d7f5ffb8f6712a04d184617f3d0f9acf5b
IsraelNatanov <israeln753951@gmail.com>
2024-02-06 22:34:06 +0200

    col-data

--- src/components/ui/tableUi.tsx
+++ src/components/ui/tableUi.tsx
@@ -6,16 +6,16 @@ import {
   ChevronLeftIcon
 } from "@chakra-ui/icons";
 interface TableProps {
-  tableSubjectData:string[]
+  tableSubjectData: string[]
   tableData: TableData[];
-  selectedRows:number[]
-  setSelectedRows:React.Dispatch<React.SetStateAction<number[]>>;
-  page:number
+  selectedRows: number[]
+  setSelectedRows: React.Dispatch<React.SetStateAction<number[]>>;
+  page: number
   setPage: React.Dispatch<React.SetStateAction<number>>;
-  setPerPage:React.Dispatch<React.SetStateAction<number>>;
+  setPerPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const TableUi = ({tableSubjectData, tableData , selectedRows, setSelectedRows,page, setPage, setPerPage}: TableProps) => {
+const TableUi = ({ tableSubjectData, tableData, selectedRows, setSelectedRows, page, setPage, setPerPage }: TableProps) => {
   const [selectAll, setSelectAll] = useState(false);
 
   const toggleSelectAll = () => {
@@ -32,7 +32,7 @@ const TableUi = ({tableSubjectData, tableData , selectedRows, setSelectedRows,pa
     const updatedSelectedRows = selectedRows.includes(id)
       ? selectedRows.filter((rowId) => rowId !== id)
       : [...selectedRows, id];
-  
+
     setSelectedRows(updatedSelectedRows);
 
     const allRowsSelected = tableData.every((item) => updatedSelectedRows.includes(item.id));
@@ -44,17 +44,16 @@ const TableUi = ({tableSubjectData, tableData , selectedRows, setSelectedRows,pa
   return (
     <TableContainer borderTopEndRadius={15} borderBottomEndRadius={15} >
       <Table variant="striped"  >
-        <Thead >
-          <Tr bg={'#3182ce'} >
+        <Thead>
+          <Tr bg={'#3182ce'}>
             <Th>
               <Checkbox isChecked={selectAll} onChange={toggleSelectAll} />
             </Th>
-            <Th textColor={'aliceblue'}>ID</Th>
-            <Th textColor={'aliceblue'}>Item</Th>
-            <Th textColor={'aliceblue'}>Price</Th>
-            <Th textColor={'aliceblue'}>Status</Th>
-            <Th textColor={'aliceblue'}> Amount of buyers</Th>
-           
+            {tableSubjectData.map((column, index) => (
+              <Th key={index} textColor={'aliceblue'}>
+                {column}
+              </Th>
+            ))}
           </Tr>
         </Thead>
         <Tbody>
@@ -68,41 +67,41 @@ const TableUi = ({tableSubjectData, tableData , selectedRows, setSelectedRows,pa
               <Td>{item.price}</Td>
               <Td>{item.status}</Td>
               <Td>{item.amountBuyers}</Td>
-              
+
             </Tr>
           ))}
         </Tbody>
 
       </Table>
       <Flex justifyContent={'flex-end'}>
-      <Tooltip label="First Page">
-            <IconButton
-  onClick={()=>page!=1? setPage(page=> page-=1):setPage(page)}
-  icon={<ChevronLeftIcon h={6} w={6} />}
-           aria-label={''}            />
-          </Tooltip>
+        <Tooltip label="First Page">
+          <IconButton
+            onClick={() => page != 1 ? setPage(page => page -= 1) : setPage(page)}
+            icon={<ChevronLeftIcon h={6} w={6} />}
+            aria-label={''} />
+        </Tooltip>
         <Select
           width={'auto'}
-       
-            onChange={(e)=> setPerPage(Number(e.target.value))}
-          >
-            {[5, 10, 15, 20].map((pageSize) => (
-              <option key={pageSize} value={pageSize}>
-               {pageSize}
-              </option>
-            ))}
-          </Select>
-      
 
-      
+          onChange={(e) => setPerPage(Number(e.target.value))}
+        >
+          {[5, 10, 15, 20].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              {pageSize}
+            </option>
+          ))}
+        </Select>
+
+
+
         <Tooltip label="Next Page">
-            <IconButton  
-              onClick={()=>setPage(page=> page+=1)}
-              // isDisabled={!canNextPage}
-              icon={<ChevronRightIcon h={6} w={6} />} aria-label={''}            />
-          </Tooltip>
-          </Flex>
-      
+          <IconButton
+            onClick={() => setPage(page => page += 1)}
+            // isDisabled={!canNextPage}
+            icon={<ChevronRightIcon h={6} w={6} />} aria-label={''} />
+        </Tooltip>
+      </Flex>
+
     </TableContainer>
   );
 };
